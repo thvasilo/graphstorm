161baa7161e490976b9dd2d04a84c7ad6d6279b7
Theodore Vasiloudis <thvasilo@amazon.com>
2024-11-23 01:38:24 +0000

    [WIP] [GSProcessing] Ensure order is maintained when processing node labels

--- graphstorm-processing/tests/test_dist_label_loader.py
+++ graphstorm-processing/tests/test_dist_label_loader.py
@@ -14,14 +14,19 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import numpy as np
+from pathlib import Path
 
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from pyarrow import parquet as pq
 from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.types import StructField, StructType, StringType
 
 
 from graphstorm_processing.data_transformations.dist_label_loader import DistLabelLoader
 from graphstorm_processing.config.label_config_base import LabelConfig
+from graphstorm_processing.constants import NODE_MAPPING_INT
 
 
 def test_dist_classification_label(spark: SparkSession, check_df_schema):
@@ -133,3 +138,86 @@ def test_dist_multilabel_classification(spark: SparkSession, check_df_schema):
             assert row_val[2 * i + 1] == 1.0
         else:
             assert i == 4, "Only the last row should be None/null"
+
+
+def test_dist_label_order_partitioned(spark: SparkSession, check_df_schema, tmp_path: Path):
+    """Test that label and mask order is maintained after label processing"""
+    label_col = "name"
+    id_col = NODE_MAPPING_INT
+    classification_config = {
+        "column": "name",
+        "type": "classification",
+        "split_rate": {"train": 0.8, "val": 0.2, "test": 0.0},
+    }
+
+    num_datapoints = 10**4
+    ids = list(range(3 * num_datapoints))
+    data_zeros = ["one" for _ in range(num_datapoints)]
+    data_ones = ["two" for _ in range(num_datapoints)]
+    data_nan = [None for _ in range(num_datapoints)]
+    data = data_zeros + data_ones + data_nan
+    pandas_input = pd.DataFrame.from_dict({label_col: data})
+    pandas_shuffled = pandas_input.sample(frac=1, random_state=42).reset_index(drop=True)
+    pandas_shuffled[id_col] = ids
+    print("pandas shuffled:")
+    print(pandas_shuffled)
+    names_df = spark.createDataFrame(pandas_shuffled)
+
+    # Create an order for the DF, then consistently shuffle it to multiple partitions
+    # names_df = names_df.withColumn("rand", F.rand(seed=54))
+    # names_df = names_df.orderBy("rand")
+    print("names_df no repart")
+    names_df.show()
+    # names_df.show()
+    print(f"{names_df.rdd.getNumPartitions()=}")
+
+    # TODO: Is this deterministic? Otherwise can add a random col
+    # and use as shuffle key
+    names_df_repart = names_df.repartition(64)
+
+    assert names_df_repart.rdd.getNumPartitions() == 64
+
+    # Convert the partitioned/shuffled DF to pandas
+    names_df_repart_pd = names_df_repart.toPandas()
+    print("names_df after repart:")
+    names_df_repart.show()
+
+    # Apply transformation in Spark
+    label_transformer = DistLabelLoader(LabelConfig(classification_config), spark)
+    transformed_labels = label_transformer.process_label(names_df_repart)
+    print("transformed labels Spark DF:")
+    transformed_labels.show()
+
+    # Apply transformation in Pandas
+    label_map = label_transformer.label_map
+    expected_transformed_pd = names_df_repart_pd.replace(
+        {
+            "one": label_map["one"],
+            "two": label_map["two"],
+        }
+    )
+    print("expected transformed pandas:")
+    print(expected_transformed_pd[:20])
+
+    out_path = str(tmp_path.joinpath("label_order_df"))
+    transformed_labels.write.mode("overwrite").parquet(out_path)
+
+    check_df_schema(transformed_labels)
+
+    print("transformed labels Pyarrow PD:")
+    actual_transformed_pd: pd.DataFrame = pq.read_table(out_path).to_pandas()
+    print(actual_transformed_pd[:20])
+
+    assert set(label_map.keys()) == {"one", "two"}
+
+    assert_frame_equal(
+        actual_transformed_pd.loc[:, ["name"]],
+        expected_transformed_pd.loc[:, ["name"]],
+        check_dtype=False,
+    )
+
+    assert_frame_equal(
+        actual_transformed_pd.sort_values(id_col).loc[:, ["name"]],
+        expected_transformed_pd.sort_values(id_col).loc[:, ["name"]],
+        check_dtype=False,
+    )
